1a16bedf754ed66b1245623c270e4b5d3c9ae18c
nata654s <nata654s@yahoo.com>
2021-02-24 21:24:44 +0200

    first commit

---
+++ app/static/app.js
@@ -0,0 +1,5 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 3, duration: 3 }, "-=1");
